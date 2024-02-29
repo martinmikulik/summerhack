352545de0990b05e8983726dac443c372a9c4e59
Martin Ivan Mikulik <martinivanmikulik@gmail.com>
2024-02-29 12:32:03 +0100

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MiPrimerComponente from './miPrimerComponente'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hola! App</h1>
+      <MiPrimerComponente></MiPrimerComponente>
+    </>
+  )
+}
+
+export default App
---
+++ src/MiPrimerComponente.jsx
@@ -0,0 +1,5 @@
+function MiPrimerComponente() {
+    return ( <h1>Mi primer Componente Holis!</h1> );
+}
+
+export default MiPrimerComponente;
